920ba81f9814266cd503492be52cd62e6fd13404
juan-montenegro <juanes9708@gmail.com>
2023-10-01 11:00:16 -0500

    Creación de proyecto

---
+++ src/main/java/com/univalle/laboratoriointerfaces2023/LaboratorioInterfaces2023.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.univalle.laboratoriointerfaces2023;
+
+/**
+ *
+ * @author juane
+ */
+public class LaboratorioInterfaces2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
